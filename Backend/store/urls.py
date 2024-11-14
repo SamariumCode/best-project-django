bb5999254492cf1ae60f3f744b4c56ad51e24acb
SamariumCode <cactuspydon@gmail.com>
2024-11-14 16:46:46 +0330

    add product_detail

--- Backend/store/urls.py
+++ Backend/store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import product_list
+from .views import product_list, product_detail
 
 urlpatterns = [
     path('products/', product_list, name='product_list'),  # Add the name to the URL pattern
+    path('products/<int:pk>/', product_detail, name='product_detail'),  # Add the name to the URL pattern
 ]
--- Backend/store/views.py
+++ Backend/store/views.py
@@ -1,6 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Product
+
 @api_view()
 def product_list(request):
     return Response("Product list")
+
+
+@api_view()
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    result = {
+        "id": product.id,
+        "name": product.name,
+        "price": product.price,
+        "description": product.description,
+
+    }
+    return Response(result)
